94dbec54cb53779100f7ecd683abf2292f4ed415
hopelessfree <781154248@qq.com>
2022-07-15 17:52:27 +0800

    发布上线

--- deploy.config.js
+++ deploy.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	app: [
     {
-    	name: 'jsplusplus_m',
+    	name: 'txclass-mobile',
     	script: 'server/index.js',
     	env: {
     		COMMON_VARIABLE: 'true'
@@ -15,10 +15,10 @@ module.exports = {
 	deploy: {
 		production: {
 			user: 'root',
-			host: '47.105.106.4',
+			host: '47.96.134.225',
 			ref: 'origin/master',
-			repo: 'https://gitee.com/jsjiajia_tishengban_admin/jsplusplus_m.git',
-			path: '/www/jsplusplus_m/production',
+			repo: 'https://github.com/hopelessfree/txclass-mobile.git',
+			path: '/www/txclass-mobile/production',
 			'pre-deploy': 'git fetch --all',
 			'post-deploy': 'npm install && npm run build && npm start && pm2 startOrRestart deploy.config.js --env production'
 		}
