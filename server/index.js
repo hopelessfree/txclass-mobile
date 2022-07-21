f0fd6c0589b4a214a5d33d22ca6338d064cb67ae
hopelessfree <781154248@qq.com>
2022-07-21 15:31:34 +0800

    test

--- server/index.js
+++ server/index.js
@@ -5,11 +5,13 @@ const homeRouter = require('./routes/index')
 
 const app = new Koa()
 
+console.log('process.env.NODE_ENV', process.env.NODE_ENV)
+
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
 config.dev = app.env !== 'production'
 
-async function start () {
+async function start() {
   // Instantiate nuxt.js
   const nuxt = new Nuxt(config)
 
